80deeb5b8d338fdeb072ead2501020987709b321
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-02-07 16:52:38 +0000

    (angular) added RoutingModule, added HomeModule, added HomeComponent

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HomeModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
