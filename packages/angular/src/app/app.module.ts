1fe32583da6ba9b0e7047c961d55cd4cf1cb5722
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-02-12 17:00:13 +0000

    (angular) removed feature modules from AppModule, style fix

--- packages/angular/src/app/app.module.ts
+++ packages/angular/src/app/app.module.ts
@@ -5,9 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing.module';
-import { HomeModule } from './pages/home/home.module';
 import { SharedModule } from './shared/shared.module';
-import { AboutModule } from './pages/about/about.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,10 +13,8 @@ import { AboutModule } from './pages/about/about.module';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    HomeModule,
     SharedModule,
-    RoutingModule,
-    AboutModule
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
