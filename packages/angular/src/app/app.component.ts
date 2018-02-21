93b5139ec60dc7789ac598483ba9452c48d84167
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-02-21 09:22:09 +0000

    (angular) added scroll to top on navigation end

--- packages/angular/src/app/app.component.ts
+++ packages/angular/src/app/app.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   template: '<header></header><router-outlet></router-outlet><footer></footer>'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  router$: Subscription;
+
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.router$ = this.router.events.subscribe(event => {
+      if (!(event instanceof NavigationEnd)) return;
+
+      this.renderer.setProperty(document.body, 'scrollTop', 0);
+    });
+  }
+
+  ngOnDestroy() {
+    this.router$.unsubscribe();
+  }
+}
