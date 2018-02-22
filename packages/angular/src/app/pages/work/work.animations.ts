c3b76eb893d57ba5a222e4b77a6ac8306bc840b8
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-02-22 15:32:39 +0000

    (angular) added hover states

--- packages/angular/src/app/pages/work/work.animations.ts
+++ packages/angular/src/app/pages/work/work.animations.ts
@@ -10,7 +10,7 @@ import {
 
 export const WorkAnimations = [
   trigger('caseStudyList', [
-    state('1', style({ transform: 'none' })),
+    state('1', style({ transform: '*' })),
     state('*', style({ transform: 'scale(0.9) translateY(40%)' })),
 
     transition('* => 1', animate('400ms ease'))
