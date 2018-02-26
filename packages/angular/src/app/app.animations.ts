335e463326e96b88a607128a46e12bb2885c3c8a
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-02-26 11:49:41 +0000

    (angular) added fade transition

--- packages/angular/src/app/app.animations.ts
+++ packages/angular/src/app/app.animations.ts
@@ -5,35 +5,30 @@ import {
   animate,
   transition,
   query,
-  group,
-  animateChild
+  group
 } from '@angular/animations';
 
 export const AppAnimations = [
   trigger('routeTransition', [
     transition('* => *', [
       group([
-        query(':enter, :leave', style({ position: 'absolute' }), {
-          optional: true
-        }),
+        query(
+          ':enter, :leave',
+          style({ position: 'absolute', left: 0, right: 0 }),
+          {
+            optional: true
+          }
+        ),
 
         query(
           ':enter',
-          [
-            style({ transform: 'translateX(100%)' }),
-            animate('0.3s ease', style({ transform: 'translateX(0)' })),
-            animateChild()
-          ],
+          [style({ opacity: 0 }), animate('0.4s ease', style({ opacity: 1 }))],
           { optional: true }
         ),
 
         query(
           ':leave',
-          [
-            style({ transform: 'translateX(0)' }),
-            animate('0.3s ease', style({ transform: 'translateX(-100%)' })),
-            animateChild()
-          ],
+          [style({ opacity: 1 }), animate('0.4s ease', style({ opacity: 0 }))],
           { optional: true }
         )
       ])
