b384b5008365943fc5120c7655ffb24b00069463
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-06-01 14:20:54 +0100

    (api) removed careers

--- packages/api/src/data/careers.ts
+++ packages/api/src/data/careers.ts
@@ -2,12 +2,6 @@ import { Api } from '../api';
 
 export const Careers: Api.Career[] = [
   {
-    title: 'Administrator'
-  },
-  {
-    title: 'Telesales Executive'
-  },
-  {
     title: 'Junior Account Manager'
   },
   {
