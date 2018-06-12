2489676ac25edf788b60df65afa2380312f5aabf
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-06-12 11:53:17 +0100

    (angular) added contact form, added contact form styling

--- packages/angular/src/app/pages/contact/contact.component.ts
+++ packages/angular/src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { TitleService } from '../../shared/shared.module';
+import { TitleService, LoggerService } from '../../shared/shared.module';
 import { ContactImages } from './contact.images';
 
 @Component({
@@ -11,10 +12,36 @@ import { ContactImages } from './contact.images';
 export class ContactComponent implements OnInit {
   images = ContactImages;
 
-  constructor(private titleService: TitleService) {}
+  contactForm: FormGroup;
 
-  messageClick() {
-    ga('send', 'event', 'Message Us Button', 'click');
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  constructor(
+    private titleService: TitleService,
+    private formBuilder: FormBuilder,
+    private logger: LoggerService
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    this.logger.log('submitForm', this.contactForm.value);
   }
 
   ngOnInit() {
--- packages/angular/src/app/pages/contact/contact.module.ts
+++ packages/angular/src/app/pages/contact/contact.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../../shared/shared.module';
 import { ContactComponent } from './contact.component';
@@ -8,7 +9,12 @@ import { ContactComponent } from './contact.component';
 const routes: Routes = [{ path: '', component: ContactComponent }];
 
 @NgModule({
-  imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
+    RouterModule.forChild(routes)
+  ],
   declarations: [ContactComponent]
 })
 export class ContactModule {}
