f68c883d9aa83e0f29446246577031e00060b90c
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-10-17 17:01:52 +0100

    ci(travis): use Hkfd CI user to push release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
+git config --global user.email "44237005+hkfd-ci@users.noreply.github.com"
+git config --global user.name "Hkfd CI"
+
 git checkout master
 git reset --hard
 
-eval "$(ssh-agent -s)"
-openssl aes-256-cbc -K $encrypted_ba5c3ba7e694_key -iv $encrypted_ba5c3ba7e694_iv -in .travis/github_deploy.enc -out github_deploy -d
-chmod 600 github_deploy
-ssh-add github_deploy
-
-git remote add release git@github.com:hkfd/hkfd.git
-lerna version --force-publish --yes --git-remote release
+git remote add release https://${GH_ACCESS_TOKEN}@github.com/hkfd/hkfd.git
+lerna version --yes --git-remote release
