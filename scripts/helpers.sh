79b8b706d7d951bc87f913a0dfaad1ca1723ed47
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-11-14 15:39:02 +0000

    ci: change log colours

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -2,15 +2,12 @@
 set -e
 
 export RESET="\e[0m \n"
-export NORMAL="\e[22m"
-export BOLD="\e[1m"
-export BLACK="\e[30m"
 export WHITE="\e[39m"
-export GREY="\e[37m"
-export GREEN="\e[32m"
-export RED="\e[31m"
-export GREY_BG="\e[47m"
-export GREEN_BG="\e[42m"
+export PASS="\e[32m"
+export FAIL="\e[31m"
+export SKIP="\e[33m"
+export PASS_BG="\e[42m"
+export SKIP_BG="\e[43m"
 
 changedPackages() {
   yarn --silent lerna ls --since 2>/dev/null
@@ -18,30 +15,30 @@ changedPackages() {
 
 isChangedPackage() {
   if changedPackages | grep -q $1; then
-    printf "${GREEN}Package ${BOLD}${WHITE}\`$1\`${NORMAL}${GREEN} has been modified${RESET}"
+    printf "${PASS}Package ${WHITE}\`$1\`${PASS} has been modified${RESET}"
     return 0;
   else
-    printf "${GREY}Package ${BOLD}\`$1\`${NORMAL} has not been modified${RESET}"
+    printf "${SKIP}Package ${WHITE}\`$1\`${SKIP} has not been modified${RESET}"
     return 1
   fi
 }
 
 isPullRequest() {
    if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-     printf "${GREY}This is not a pull request${RESET}"
+     printf "${SKIP}This is not a pull request${RESET}"
      return 1;
    else
-     printf "${GREEN}This is a pull request${RESET}"
+     printf "${PASS}This is a pull request${RESET}"
      return 0
    fi
 }
 
 shouldRunTest() {
   if isPullRequest || isChangedPackage $1; then
-    printf "${GREEN_BG}${BOLD}${WHITE} Running test ${RESET}"
+    printf "${PASS_BG} Running test ${RESET}"
     return 0;
   else
-    printf "${GREY_BG}${BOLD}${BLACK} Not running test ${RESET}"
+    printf "${SKIP_BG} Not running test ${RESET}"
     return 1;
   fi
 }
@@ -58,11 +55,11 @@ shouldRunDeploy() {
   checkoutPreviousCommit
 
   if isChangedPackage $1; then
-    printf "${GREEN_BG}${BOLD}${WHITE} Deploying ${RESET}"
+    printf "${PASS_BG} Deploying ${RESET}"
     resetToReleaseCommit
     return 0;
   else
-    printf "${GREY_BG}${BOLD}${BLACK} Not deploying ${RESET}"
+    printf "${SKIP_BG} Not deploying ${RESET}"
     return 1;
   fi
 }
