74f52837b18410d67bb5ec5513caf4ee0d6a0630
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-11-14 11:10:16 +0000

    ci: replace `lerna` -> `yarn`

--- scripts/angular/spec-server.sh
+++ scripts/angular/spec-server.sh
@@ -4,6 +4,6 @@ source ./scripts/angular/.env
 source ./scripts/helpers.sh
 
 if shouldRunTest $PACKAGE; then
-  ENVIRONMENT=staging lerna run build:ssr --scope=$PACKAGE
+  ENVIRONMENT=staging yarn --cwd packages/$PACKAGE build:ssr
   yarn --cwd packages/$PACKAGE test:server --coverage --coverageReporters=text-lcov | yarn coveralls
 fi
