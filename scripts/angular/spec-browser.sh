675d2c07b8c7b6815b763f0bb54275bfdcd77d85
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-11-15 17:01:21 +0000

    ci: enable `shouldRunTest` for angular browser spec

--- scripts/angular/spec-browser.sh
+++ scripts/angular/spec-browser.sh
@@ -3,7 +3,7 @@ set -e
 source ./scripts/angular/.env
 source ./scripts/helpers.sh
 
-# if shouldRunTest $PACKAGE; then
+if shouldRunTest $PACKAGE; then
   yarn --cwd packages/$PACKAGE test:browser --progress=false --browsers=HeadlessChromeNoSandbox --code-coverage
   cat packages/$PACKAGE/coverage/lcov.info | yarn coveralls
-# fi
+fi
