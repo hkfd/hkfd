c41af31b50cd51129e5612a97b05ab880757ffbe
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2018-11-16 09:46:39 +0000

    ci: switch code coverage to codecov

--- scripts/email/test.sh
+++ scripts/email/test.sh
@@ -4,5 +4,6 @@ source ./scripts/email/.env
 source ./scripts/helpers.sh
 
 if shouldRunTest $PACKAGE; then
-  yarn --cwd packages/$PACKAGE test --coverage --coverageReporters=text-lcov | yarn coveralls
+  yarn --cwd packages/$PACKAGE test --coverage
+  bash <(curl -s https://codecov.io/bash)
 fi
