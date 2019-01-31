fb2ccce484a2b4a6b94be4b1e19f69029c7e50d1
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2019-01-31 12:27:38 +0000

    ci: remove `shouldRunRelease` check
    Previously, `shouldRunRelease` would check the commit type, and only run `release` if the type was either feat, fix, perf, or refactor. This check was only run on the latest commit, which works if the pr only includes a single commit or the top commit is within the range. There doesn't seem to be a way to get the commit range on circle, so this commit removes this check and will now run the release stage on every push to master

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -e
-source ./scripts/helpers.sh
 
-release() {
-  git config --global user.email "44237005+hkfd-ci@users.noreply.github.com"
-  git config --global user.name "Hkfd CI"
+git config --global user.email "44237005+hkfd-ci@users.noreply.github.com"
+git config --global user.name "Hkfd CI"
 
-  git checkout master
-  git reset --hard
+git checkout master
+git reset --hard
 
-  git remote add release https://${GH_ACCESS_TOKEN}@github.com/hkfd/hkfd.git
-  yarn lerna version --yes --git-remote release
-}
-
-if shouldRunRelease; then
-  release
-fi
+git remote add release https://${GH_ACCESS_TOKEN}@github.com/hkfd/hkfd.git
+yarn lerna version --yes --git-remote release
