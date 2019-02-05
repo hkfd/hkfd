592b0e966f796590b41b9d99e0dc64a3f677599b
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2019-02-05 09:47:21 +0000

    ci: lock chromedriver version for angular e2e
    Fixes 'Chrome version must be...' error

--- scripts/angular/e2e.sh
+++ scripts/angular/e2e.sh
@@ -4,5 +4,6 @@ source ./scripts/angular/.env
 source ./scripts/helpers.sh
 
 if shouldRunTest $PACKAGE; then
-  yarn --cwd packages/$PACKAGE e2e --protractor-config=protractor-ci.conf.js
+  yarn webdriver-manager update --standalone false --gecko false --versions.chrome 2.37
+  yarn --cwd packages/$PACKAGE e2e --protractor-config=protractor-ci.conf.js --webdriver-update=false
 fi
