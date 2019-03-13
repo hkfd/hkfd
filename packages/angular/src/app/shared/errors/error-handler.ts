b9900537635297d9c363eb0c15271addbf18dd26
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2019-03-13 10:27:36 +0000

    fix(angular): call Sentry in production only

--- packages/angular/src/app/shared/errors/error-handler.ts
+++ packages/angular/src/app/shared/errors/error-handler.ts
@@ -6,13 +6,11 @@ import { environment } from 'environment';
 
 export class GlobalErrorHandler implements ErrorHandler {
   constructor() {
-    if (!environment.production) return;
-
-    Raven.config(environment.sentry.dsn).install();
+    environment.production && Raven.config(environment.sentry.dsn).install();
   }
 
   handleError(err: any) {
     console.error(err);
-    Raven.captureException(err);
+    environment.production && Raven.captureException(err);
   }
 }
