774fd27a3fb605de8adca147c84bd671b19ba561
Rajan Solanki <33425636+heckfordraj@users.noreply.github.com>
2019-07-10 14:41:10 +0100

    refactor(angular): add `createMetaTags` helper

--- packages/angular/src/app/shared/meta.service.helpers.spec.ts
+++ packages/angular/src/app/shared/meta.service.helpers.spec.ts
@@ -1,4 +1,4 @@
-import { createTitle } from './meta.service.helpers';
+import { createTitle, createMetaTags } from './meta.service.helpers';
 
 describe('`createTitle`', () => {
   it('should return default title if not passed `title`', () => {
@@ -11,5 +11,113 @@ describe('`createTitle`', () => {
     const res = createTitle({ title: 'Title' });
 
     expect(res).toBe('Heckford – Title');
+  });
+});
+
+describe('`createMetaTags`', () => {
+  describe('`type`', () => {
+    it('should return `type` if passed `tags.type`', () => {
+      const { type } = createMetaTags({ type: 'type' as any });
+
+      expect(type).toBe('type');
+    });
+
+    it('should return default `type` if passed `tags.type` is `undefined`', () => {
+      const { type } = createMetaTags({ type: undefined });
+
+      expect(type).toBe('website');
+    });
+
+    it('should return default `type` if not passed `tags.type`', () => {
+      const { type } = createMetaTags({});
+
+      expect(type).toBe('website');
+    });
+  });
+
+  describe('`title`', () => {
+    it('should return `title` if passed `tags.title`', () => {
+      const { title } = createMetaTags({ title: 'title' as any });
+
+      expect(title).toBe('title');
+    });
+
+    it('should return default `title` if passed `tags.title` is `undefined`', () => {
+      const { title } = createMetaTags({ title: undefined });
+
+      expect(title).toBe('Heckford');
+    });
+
+    it('should return default `title` if not passed `tags.title`', () => {
+      const { title } = createMetaTags({});
+
+      expect(title).toBe('Heckford');
+    });
+  });
+
+  describe('`description`', () => {
+    it('should return `description` if passed `tags.description`', () => {
+      const { description } = createMetaTags({
+        description: 'description' as any
+      });
+
+      expect(description).toBe('description');
+    });
+
+    it('should return default `description` if passed `tags.description` is `undefined`', () => {
+      const { description } = createMetaTags({ description: undefined });
+
+      expect(description).toBe('Independent advertising & marketing agency');
+    });
+
+    it('should return default `description` if not passed `tags.description`', () => {
+      const { description } = createMetaTags({});
+
+      expect(description).toBe('Independent advertising & marketing agency');
+    });
+  });
+
+  describe('`image`', () => {
+    it('should return `image` if passed `tags.image`', () => {
+      const { image } = createMetaTags({
+        image: 'image'
+      });
+
+      expect(image).toBe('image');
+    });
+
+    it('should return default `image` if passed `tags.image` is `undefined`', () => {
+      const { image } = createMetaTags({ image: undefined });
+
+      expect(image).toBe('https://testing/assets/heckford.png');
+    });
+
+    it('should return default `image` if not passed `tags.image`', () => {
+      const { image } = createMetaTags({});
+
+      expect(image).toBe('https://testing/assets/heckford.png');
+    });
+  });
+
+  describe('`url`', () => {
+    it('should return combined `environment.deploy.url` and`url` if passed `tags.url`', () => {
+      const { url } = createMetaTags({
+        url: 'url'
+      });
+
+      expect(url).toBe('https://testing/url');
+    });
+
+    it('should return default `url` if passed `tags.url` is `undefined`', () => {
+      const { url } = createMetaTags({ url: undefined });
+
+      expect(url).toBe('https://testing/');
+    });
+
+    it('should return default `url` if not passed `tags.url`', () => {
+      const { url } = createMetaTags({});
+
+      expect(url).toBe('https://testing/');
+    });
   });
 });
--- packages/angular/src/app/shared/meta.service.helpers.ts
+++ packages/angular/src/app/shared/meta.service.helpers.ts
@@ -1,4 +1,19 @@
+import { environment } from 'environment';
 import { MetaTags } from 'shared';
 
 export const createTitle = ({ title }: Partial<MetaTags>): string =>
   `Heckford${title ? ` – ${title}` : ''}`;
+
+export const createMetaTags = ({
+  type = 'website',
+  title = 'Heckford',
+  description = 'Independent advertising & marketing agency',
+  image = `${environment.deployUrl}assets/heckford.png`,
+  url = ''
+}: Partial<MetaTags>): MetaTags => ({
+  type,
+  title,
+  description,
+  image,
+  url: `${environment.deployUrl}${url}`
+});
